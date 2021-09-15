292db09a7a09359cfbfedf3bb453bede2d4b20d5
atlgithub2308 <89908586+atlgithub2308@users.noreply.github.com>
2021-09-15 15:13:00 +0800

    Update puppet_role.rb

--- lib/facter/puppet_role.rb
+++ lib/facter/puppet_role.rb
@@ -1,4 +1,9 @@
 Facter.add('role') do
+  
+  confine :kernel do |value|
+    value == "Linux"
+  end
+  
   setcode do
     Facter::Core::Execution.execute('/bin/cat /opt/puppetlabs/puppet/cache/state/classes.txt |grep -i roles | cut -c8-')
   end
